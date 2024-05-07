e7bae7bb27de5da23543962879c6e6a552e96e4b
이건창 <92219795+this-is-spear@users.noreply.github.com>
2024-05-08 01:13:02 +0900

    feature : GiftCouponService 구현 및 테스트

---
+++ src/main/kotlin/com/example/estdelivery/application/GiftCouponService.kt
@@ -0,0 +1,49 @@
+package com.example.estdelivery.application
+
+import com.example.estdelivery.application.port.`in`.GiftCouponUseCase
+import com.example.estdelivery.application.port.`in`.command.GiftCouponCommand
+import com.example.estdelivery.application.port.out.LoadCouponStatePort
+import com.example.estdelivery.application.port.out.LoadMemberStatePort
+import com.example.estdelivery.application.port.out.UpdateMemberStatePort
+import com.example.estdelivery.application.port.out.state.MemberState
+import com.example.estdelivery.application.utils.TransactionArea
+import com.example.estdelivery.domain.coupon.Coupon
+import com.example.estdelivery.domain.member.Member
+
+class GiftCouponService(
+    loadMemberStatePort: LoadMemberStatePort,
+    loadCouponStatePort: LoadCouponStatePort,
+    updateMemberStatePort: UpdateMemberStatePort,
+    private val transactionArea: TransactionArea,
+    private val getReceiver: (GiftCouponCommand) -> Member = { loadMemberStatePort.findById(it.receiverId).toMember() },
+    private val getSender: (GiftCouponCommand) -> Member = { loadMemberStatePort.findById(it.senderId).toMember() },
+    private val getCoupon: (GiftCouponCommand) -> Coupon = { loadCouponStatePort.findByCouponId(it.couponId).toCoupon() },
+    private val updateMember: (Member) -> Unit = { updateMemberStatePort.update(MemberState.from(it)) },
+) : GiftCouponUseCase {
+    /**
+     * 선물 할 쿠폰과 선물 할 회원의 이름을 입력해 쿠폰을 나눠준다.
+     *
+     * 1. 이름으로 회원을 조회한다.
+     * 2. 선물 할 쿠폰을 조회한다.
+     * 3. 선물받을 회원 쿠폰북에 해당 쿠폰을 추가한다.
+     * 4. 선물한 회원 쿠폰북에 해당 쿠폰을 삭제한다.
+     *
+     * @param giftCouponCommand 선물할 쿠폰 정보와 선물할 회원 정보
+     */
+    override fun giftCoupon(giftCouponCommand: GiftCouponCommand) {
+        val receiver = getReceiver(giftCouponCommand)
+
+        transactionArea.run {
+            val sender = getSender(giftCouponCommand)
+
+            if (sender == receiver) {
+                throw IllegalArgumentException("자기 자신에게 쿠폰을 선물할 수 없습니다.")
+            }
+
+            val coupon = getCoupon(giftCouponCommand)
+            sender.sendCoupon(coupon, receiver)
+            updateMember(receiver)
+            updateMember(sender)
+        }
+    }
+}
---
+++ src/test/kotlin/com/example/estdelivery/application/GiftCouponServiceTest.kt
@@ -0,0 +1,98 @@
+package com.example.estdelivery.application
+
+import com.example.estdelivery.application.port.`in`.command.GiftCouponCommand
+import com.example.estdelivery.application.port.out.LoadCouponStatePort
+import com.example.estdelivery.application.port.out.LoadMemberStatePort
+import com.example.estdelivery.application.port.out.UpdateMemberStatePort
+import com.example.estdelivery.application.port.out.state.CouponState
+import com.example.estdelivery.application.port.out.state.CouponStateAmountType
+import com.example.estdelivery.application.port.out.state.CouponStateType
+import com.example.estdelivery.application.port.out.state.MemberState
+import com.example.estdelivery.application.utils.TransactionArea
+import io.kotest.assertions.throwables.shouldNotThrow
+import io.kotest.assertions.throwables.shouldThrow
+import io.kotest.core.spec.style.FreeSpec
+import io.mockk.every
+import io.mockk.mockk
+
+class GiftCouponServiceTest : FreeSpec({
+    val loadMemberStatePort = mockk<LoadMemberStatePort>()
+    val loadCouponStatePort = mockk<LoadCouponStatePort>()
+    val updateMemberStatePort = mockk<UpdateMemberStatePort>()
+    val transactionArea = TransactionArea()
+    lateinit var giftCouponService: GiftCouponService
+
+    beforeTest {
+        giftCouponService = GiftCouponService(
+            loadMemberStatePort,
+            loadCouponStatePort,
+            updateMemberStatePort,
+            transactionArea,
+        )
+    }
+
+    "회원은 주변 회원에게 쿠폰을 선물할 수 있다" - {
+        // given
+        val receiverId = 3L
+        val senderId = 2L
+        val giftCouponCommand = GiftCouponCommand(1L, senderId, receiverId)
+        val couponState = CouponState(
+            "쿠폰",
+            "나눠줄 쿠폰",
+            CouponStateAmountType.FIX,
+            CouponStateType.HANDOUT,
+            1000,
+            1L
+        )
+        val receiverCouponBook = listOf(couponState.toCoupon())
+
+        every { loadMemberStatePort.findById(senderId) } returns MemberState("주는자", receiverCouponBook, senderId)
+        every { loadMemberStatePort.findById(receiverId) } returns MemberState("받는자", listOf(), receiverId)
+        every { loadCouponStatePort.findByCouponId(giftCouponCommand.couponId) } returns couponState
+        every { updateMemberStatePort.update(any()) } returns Unit
+
+        // when &  then
+        shouldNotThrow<Exception> {
+            giftCouponService.giftCoupon(giftCouponCommand)
+        }
+    }
+
+    "쿠폰이 존재해야 하고 사용하지 않은 쿠폰이어야 한다." {
+        // given
+        val receiverId = 3L
+        val senderId = 2L
+        val giftCouponCommand = GiftCouponCommand(1L, senderId, receiverId)
+
+        every { loadMemberStatePort.findById(senderId) } returns MemberState("주는자",  listOf(), senderId)
+        every { loadMemberStatePort.findById(receiverId) } returns MemberState("받는자", listOf(), receiverId)
+
+        // when &  then
+        shouldThrow<IllegalArgumentException> {
+            giftCouponService.giftCoupon(giftCouponCommand)
+        }
+    }
+
+    "동일한 회원이어서는 안된다." {
+        // given
+        val receiverId = 3L
+        val senderId = 3L
+        val giftCouponCommand = GiftCouponCommand(1L, senderId, receiverId)
+        val couponState = CouponState(
+            "쿠폰",
+            "나눠줄 쿠폰",
+            CouponStateAmountType.FIX,
+            CouponStateType.HANDOUT,
+            1000,
+            1L
+        )
+        val receiverCouponBook = listOf(couponState.toCoupon())
+
+        every { loadMemberStatePort.findById(senderId) } returns MemberState("주는자", receiverCouponBook, senderId)
+        every { loadMemberStatePort.findById(receiverId) } returns MemberState("받는자", listOf(), receiverId)
+
+        // when &  then
+        shouldThrow<Exception> {
+            giftCouponService.giftCoupon(giftCouponCommand)
+        }
+    }
+})
