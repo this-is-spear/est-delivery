c161b08d88c05417eaf2530b24cfe3725391ca86
이건창 <92219795+this-is-spear@users.noreply.github.com>
2024-05-23 19:40:09 +0900

    refactor : GiftCouponMessageController 상수 값 추가

--- coupon/src/main/kotlin/com/example/estdelivery/coupon/application/port/in/web/GiftCouponMessageController.kt
+++ coupon/src/main/kotlin/com/example/estdelivery/coupon/application/port/in/web/GiftCouponMessageController.kt
@@ -1,5 +1,6 @@
 package com.example.estdelivery.coupon.application.port.`in`.web
 
+import com.example.estdelivery.coupon.application.port.`in`.EnrollCouponByMessageUseCase
 import com.example.estdelivery.coupon.application.port.`in`.FindAvailableGiftCouponUseCase
 import com.example.estdelivery.coupon.application.port.`in`.GiftCouponByMessageUseCase
 import com.example.estdelivery.coupon.application.port.`in`.web.dto.GiftCouponResponse
@@ -7,6 +8,7 @@ import com.example.estdelivery.coupon.application.port.`in`.web.dto.GiftCouponRe
 import com.example.estdelivery.coupon.application.port.`in`.web.dto.GiftMessageResponse
 import com.example.estdelivery.coupon.domain.coupon.Coupon.FixDiscountCoupon
 import com.example.estdelivery.coupon.domain.coupon.Coupon.RateDiscountCoupon
+import com.example.estdelivery.coupon.domain.coupon.GiftCouponCode
 import java.net.URL
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
@@ -16,14 +18,17 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
+private const val MEMBER_ID = "Member-ID"
+
 @RestController
 @RequestMapping("/gift-coupons")
 class GiftCouponMessageController(
     private val findAvailableGiftCouponUseCase: FindAvailableGiftCouponUseCase,
     private val giftCouponByMessageUseCase: GiftCouponByMessageUseCase,
+    private val enrollCouponByMessageUseCase: EnrollCouponByMessageUseCase,
 ) {
     @GetMapping
-    fun findAvailableGiftCoupons(@RequestHeader(value = "Member-ID") memberId: Long): GiftCouponResponses =
+    fun findAvailableGiftCoupons(@RequestHeader(value = MEMBER_ID) memberId: Long): GiftCouponResponses =
         findAvailableGiftCouponUseCase.findAvailableGiftCoupon(memberId)
             .map {
                 GiftCouponResponse(
@@ -39,7 +44,7 @@ class GiftCouponMessageController(
 
     @PostMapping("/send/{couponId}")
     fun sendGiftAvailableCoupon(
-        @RequestHeader(value = "Member-ID") memberId: Long,
+        @RequestHeader(value = MEMBER_ID) memberId: Long,
         @RequestParam message: String,
         @PathVariable couponId: Long,
     ): GiftMessageResponse =
@@ -54,4 +59,10 @@ class GiftCouponMessageController(
                 enrollHref = URL("http", "localhost", 8080, "/gift-coupons/enroll/${it.giftCouponCode.code}")
             )
         }
+
+    @GetMapping("/enroll/{code}")
+    fun enrollGiftCoupon(
+        @RequestHeader(value = MEMBER_ID) memberId: Long,
+        @PathVariable code: String
+    ) = enrollCouponByMessageUseCase.enroll(memberId, GiftCouponCode(code))
 }
