d4df312c82ae6404ff2b486adedcde298a7674b6
이건창 <92219795+this-is-spear@users.noreply.github.com>
2024-06-02 23:24:22 +0900

    fix : regex 실패하는 계약서 수정

--- shop/src/test/resources/contracts/shop/addRoyalCustomers.kts
+++ shop/src/test/resources/contracts/shop/addRoyalCustomers.kts
@@ -3,13 +3,12 @@ package shop
 import org.springframework.cloud.contract.spec.ContractDsl.Companion.contract
 
 
-
 arrayOf(
     contract {
         name = "add royal customers"
         request {
             method = POST
-            url = url(v(regex("/owners/shop/[0-9]{1,8}/royal-customers/[0-9]{1,7}[13579]")))
+            url = url(v(regex("/owners/shop/[0-9]{1,8}/royal-customers/[1-9]{0,7}[13579]")))
         }
         response {
             status = OK
@@ -18,7 +17,7 @@ arrayOf(
     contract {
         request {
             method = POST
-            url = url(v(regex("/owners/shop/[0-9]{1,8}/royal-customers/[0-9]{1,7}[02468]")))
+            url = url(v(regex("/owners/shop/[0-9]{1,8}/royal-customers/[1-9]{0,7}[02468]")))
         }
         response {
             status = BAD_REQUEST
