3157c9038cc3af6ab7e04971ff99c17fd5958658
이건창 <92219795+this-is-spear@users.noreply.github.com>
2024-06-04 22:50:17 +0900

    test(coupon): member 계약 변경으로 인한 테스트 추가

--- coupon/src/test/kotlin/com/example/estdelivery/coupon/application/port/out/adapter/infra/MemberInfraAdapterTest.kt
+++ coupon/src/test/kotlin/com/example/estdelivery/coupon/application/port/out/adapter/infra/MemberInfraAdapterTest.kt
@@ -2,10 +2,13 @@ package com.example.estdelivery.coupon.application.port.out.adapter.infra
 
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.cloud.contract.stubrunner.spring.AutoConfigureStubRunner
 import org.springframework.cloud.contract.stubrunner.spring.StubRunnerProperties
+import org.springframework.web.client.HttpClientErrorException
+import org.springframework.web.client.HttpServerErrorException
 
 
 @SpringBootTest
@@ -23,5 +26,33 @@ class MemberInfraAdapterTest(
         val member = memberInfraAdapter.findMember(1L)
         assertThat(member.id).isEqualTo(1L)
         assertThat(member.name).isEqualTo("이건창")
+    }
+
+    @Test
+    fun `1000번 식별자를 조회하면 400 에러가 발생한다`() {
+        assertThrows<HttpClientErrorException.BadRequest> {
+            memberInfraAdapter.findMember(1000)
+        }
+    }
+
+    @Test
+    fun `1001번 식별자를 조회하면 429 에러가 발생한다`() {
+        assertThrows<HttpClientErrorException.TooManyRequests> {
+            memberInfraAdapter.findMember(1001)
+        }
+    }
+
+    @Test
+    fun `1100번 식별자를 조회하면 500 에러가 발생한다`() {
+        assertThrows<HttpServerErrorException.InternalServerError> {
+            memberInfraAdapter.findMember(1100)
+        }
+    }
+
+    @Test
+    fun `1101번 식별자를 조회하면 503 에러가 발생한다`() {
+        assertThrows<HttpServerErrorException.ServiceUnavailable> {
+            memberInfraAdapter.findMember(1101)
+        }
     }
 }
