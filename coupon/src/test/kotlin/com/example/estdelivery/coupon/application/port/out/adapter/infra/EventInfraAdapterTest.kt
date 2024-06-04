74954649cd868cbdecc4db7dea05468aa1ebbf67
이건창 <92219795+this-is-spear@users.noreply.github.com>
2024-06-04 22:49:41 +0900

    test(coupon): event 계약 변경으로 인한 테스트 추가

--- coupon/src/test/kotlin/com/example/estdelivery/coupon/application/port/out/adapter/infra/EventInfraAdapterTest.kt
+++ coupon/src/test/kotlin/com/example/estdelivery/coupon/application/port/out/adapter/infra/EventInfraAdapterTest.kt
@@ -3,10 +3,15 @@ package com.example.estdelivery.coupon.application.port.out.adapter.infra
 import com.example.estdelivery.coupon.application.port.out.adapter.infra.dto.UpdateParticipatedMembersState
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertAll
+import org.junit.jupiter.api.assertThrows
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.cloud.contract.stubrunner.spring.AutoConfigureStubRunner
 import org.springframework.cloud.contract.stubrunner.spring.StubRunnerProperties
+import org.springframework.web.client.HttpClientErrorException
+import org.springframework.web.client.HttpClientErrorException.BadRequest
+import org.springframework.web.client.HttpServerErrorException
 
 @SpringBootTest
 @AutoConfigureStubRunner(
@@ -27,5 +32,65 @@ class EventInfraAdapterTest(
     fun `회원을 이벤트에 참가시킨다`() {
         val participatedMembersState = UpdateParticipatedMembersState(1, 1)
         eventInfraAdapter.participateMember(participatedMembersState)
+    }
+
+    @Test
+    fun `1000번 식별자를 조회하면 400 에러가 발생한다`() {
+        assertAll(
+            { assertThrows<BadRequest> { eventInfraAdapter.findEvent(1000) } },
+            {
+                assertThrows<BadRequest> {
+                    eventInfraAdapter.participateMember(UpdateParticipatedMembersState(1, 1000))
+                }
+            }
+        )
+    }
+
+    @Test
+    fun `1001번 식별자를 조회하면 429 에러가 발생한다`() {
+        assertAll(
+            {
+                assertThrows<HttpClientErrorException.TooManyRequests> {
+                    eventInfraAdapter.findEvent(1001)
+                }
+            },
+            {
+                assertThrows<HttpClientErrorException.TooManyRequests> {
+                    eventInfraAdapter.participateMember(UpdateParticipatedMembersState(1, 1001))
+                }
+            }
+        )
+    }
+
+    @Test
+    fun `1100번 식별자를 조회하면 500 에러가 발생한다`() {
+        assertAll(
+            {
+                assertThrows<HttpServerErrorException.InternalServerError> {
+                    eventInfraAdapter.findEvent(1100)
+                }
+            },
+            {
+                assertThrows<HttpServerErrorException.InternalServerError> {
+                    eventInfraAdapter.participateMember(UpdateParticipatedMembersState(1, 1100))
+                }
+            }
+        )
+    }
+
+    @Test
+    fun `1101번 식별자를 조회하면 503 에러가 발생한다`() {
+        assertAll(
+            {
+                assertThrows<HttpServerErrorException.ServiceUnavailable> {
+                    eventInfraAdapter.findEvent(1101)
+                }
+            },
+            {
+                assertThrows<HttpServerErrorException.ServiceUnavailable> {
+                    eventInfraAdapter.participateMember(UpdateParticipatedMembersState(1, 1101))
+                }
+            }
+        )
     }
 }
