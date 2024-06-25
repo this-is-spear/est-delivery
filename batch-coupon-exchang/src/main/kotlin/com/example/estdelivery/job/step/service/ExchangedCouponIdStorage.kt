2be83c15921d86dcf7efa8aa709e124182bcf83b
이건창 <92219795+this-is-spear@users.noreply.github.com>
2024-06-25 23:28:07 +0900

    feature(batch) : 시도 중 실패하는 경우 생성된 쿠폰 ID로 이후 작업 처리할 수 있도록 주입 방법 추가

--- batch-coupon-exchang/src/main/kotlin/com/example/estdelivery/job/step/service/ExchangedCouponIdStorage.kt
+++ batch-coupon-exchang/src/main/kotlin/com/example/estdelivery/job/step/service/ExchangedCouponIdStorage.kt
@@ -1,12 +1,18 @@
 package com.example.estdelivery.job.step.service
 
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Component
 
 @Component
 class ExchangedCouponIdStorage {
+    @Value("\${batch.expire-coupon-id}")
     var couponId: Long = 0
         get() {
             require(field != 0L) { "CouponId is not set" }
             return field
+        }
+        set(value) {
+            require(field == 0L) { "CouponId is already set" }
+            field = value
         }
 }
