d38f5a3280774ee6e30d4e35c99f45d0e1f790ac
이건창 <92219795+this-is-spear@users.noreply.github.com>
2024-07-21 13:18:44 +0900

    feature(batch) : ExchangeCouponSteps 에서 알림톡 템플릿 구현

--- batch-coupon-exchang/src/main/kotlin/com/example/estdelivery/job/step/ExchangeCouponSteps.kt
+++ batch-coupon-exchang/src/main/kotlin/com/example/estdelivery/job/step/ExchangeCouponSteps.kt
@@ -1,6 +1,7 @@
 package com.example.estdelivery.job.step
 
 import com.example.estdelivery.ChunkSizeProperty
+import com.example.estdelivery.domain.CouponExchangeNotificationTemplate
 import com.example.estdelivery.domain.CouponStateAmountType
 import com.example.estdelivery.domain.MemberCoupon
 import com.example.estdelivery.job.step.service.CouponService
@@ -9,6 +10,7 @@ import org.springframework.batch.core.configuration.annotation.JobScope
 import org.springframework.batch.core.repository.JobRepository
 import org.springframework.batch.core.step.builder.StepBuilder
 import org.springframework.batch.item.ItemProcessor
+import org.springframework.batch.item.ItemWriter
 import org.springframework.batch.item.database.JpaCursorItemReader
 import org.springframework.batch.item.database.JpaItemWriter
 import org.springframework.batch.repeat.RepeatStatus.FINISHED
@@ -56,16 +58,34 @@ class ExchangeCouponSteps {
             val createdCouponId = couponService.createCouponToBeExchange(name, description, amountType, amount)
             val jobExecutionId = chunkContext.stepContext.stepExecution.jobExecutionId
             couponService.createExchangeHistory(expiredCouponId, createdCouponId, jobExecutionId)
-            exchangedCouponIdStorage.couponId = createdCouponId
+            exchangedCouponIdStorage.couponIdAfterExchange = createdCouponId
             FINISHED
         }, transactionManager)
         .build()
 
+    /**
+     * 회원 정보 중 휴대폰 정보를 조회해 알림톡을 전송한다.
+     * 1. 교환한 사용자 쿠폰([MemberCoupon]) 리스트,
+     * 회원 정보([com.example.estdelivery.client.MemberResponse]),
+     * 쿠폰 정보([com.example.estdelivery.domain.Coupon])를 조회한다.
+     * 2. 어떤 쿠폰이 어떻게 변경됐는지 메시지를 작성한다.
+     * 3. 알림톡을 전송한다.
+     *
+     * 도중에 실패하면 어떡하지?
+     * 다시 시도할 때 남은 영역만 시도할 수 있는 방법이 있을까?
+     */
     @Bean
     fun alimTalkStep(
         jobRepository: JobRepository,
         transactionManager: PlatformTransactionManager,
+        chunkSizeProperty: ChunkSizeProperty,
+        memberCouponReader: JpaCursorItemReader<MemberCoupon>,
+        createExchangeNotificationTemplateProcessors: ItemProcessor<MemberCoupon, CouponExchangeNotificationTemplate>,
+        notificationWriter: ItemWriter<CouponExchangeNotificationTemplate>,
     ) = StepBuilder(EXCHANGE_COUPON_ALIM_TALK, jobRepository)
-        .tasklet({ _, _ -> FINISHED }, transactionManager)
+        .chunk<MemberCoupon, CouponExchangeNotificationTemplate>(chunkSizeProperty.commitSize, transactionManager)
+        .reader(memberCouponReader)
+        .processor(createExchangeNotificationTemplateProcessors)
+        .writer(notificationWriter)
         .build()
 }
