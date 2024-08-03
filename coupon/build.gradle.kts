deba6ce9803fbde0c7bda1b1cbfb064166f0c9c0
이건창 <92219795+this-is-spear@users.noreply.github.com>
2024-08-03 21:18:37 +0900

    chore(coupon) : jib task 생성

--- coupon/build.gradle.kts
+++ coupon/build.gradle.kts
@@ -10,3 +10,29 @@ dependencies {
     testImplementation("org.springframework.cloud:spring-cloud-starter-contract-stub-runner")
     testImplementation("org.springframework.cloud:spring-cloud-contract-spec-kotlin")
 }
+
+val dockerUsername: String = System.getProperty("DOCKER_USERNAME")
+val dockerPassword: String = System.getProperty("DOCKER_PASSWORD")
+
+jib {
+    from {
+        image = "openjdk:17.0.2-slim"
+        platforms {
+            platform {
+                architecture = "arm64"
+                os = "linux"
+            }
+        }
+    }
+    to {
+        image = "geonc123/tis-${project.name}"
+        auth {
+            username = dockerUsername
+            password = dockerPassword
+        }
+        tags = setOf("latest", project.version.toString().lowercase())
+    }
+    container {
+        jvmFlags = listOf("-Xms256m", "-Xmx512m")
+    }
+}
